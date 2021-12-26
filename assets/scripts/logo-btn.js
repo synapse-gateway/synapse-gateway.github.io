f3528a50061909896cfa4e65741c8a2fc13614f3
Jay Gudsson <gudsson@gmail.com>
2021-12-26 00:40:07 -0800

    adds hover effect to logo home button

---
+++ assets/scripts/logo-btn.js
@@ -0,0 +1,7 @@
+function hover(element) {
+  element.setAttribute("src", "assets/images/logo-mono.png");
+}
+
+function unhover(element) {
+  element.setAttribute("src", "assets/images/logo-color.png");
+}
